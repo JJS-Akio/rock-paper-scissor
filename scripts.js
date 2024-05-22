36ac50145c065c5c4311636cfe4070f236ac23a1
JJS-Akio <sanchezjosue96@gmail.com>
2024-05-22 18:40:07 +0900

    completed rock paper scissors with prompts

--- scripts.js
+++ scripts.js
@@ -15,16 +15,42 @@ function getUserPrompt (){
     return(userinput);
 }
 
-function outcomeGame (){
-    let userChoice = getUserPrompt();
-    let computerChoice = getComputerChoice();
+function  playRound(userChoice, computerChoice){
     console.log(`Your choice: ${userChoice}, Computer's choice: ${computerChoice}`);
     if(userChoice === computerChoice){
         return "TIE";
     } else if ( userChoice === "paper" && computerChoice === "rock" || userChoice === "rock" && computerChoice === "scissors" || userChoice === "scissors" && computerChoice === "paper"){
-        return "You win";
+        return "Take is slow and breath, next might not be as easy";
     } else{
-        return "You lose";
+        return "You blind or what, do not lose the next round";
     }
 }
-console.log(outcomeGame())
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
+        const humanSelection = getUserPrompt();
+        const computerSelection = getComputerChoice();
+        const result = playRound(humanSelection, computerSelection);
+        alert(result);
+        if (result === "Take is slow and breath, next might not be as easy") {
+            humanScore++;
+        } else if (result === "You blind or what, do not lose the next round") {
+            computerScore++;
+        }
+    }
+
+    alert(`Final score - You: ${humanScore}, Computer: ${computerScore}`);
+    if (humanScore > computerScore) {
+        alert("Close one, robots won't rule yet!");
+    } else if (humanScore < computerScore) {
+        alert("Computer wins, well AI will probably take over.");
+    } else {
+        alert("Probably should not happen but if it did we are in a matrix");
+    }
+}
+
+playGame();
+console.log(playGame)
